c07c010b067196d1770cb312bb5a3fd27383f884
baharehpourmalayeri <baharehpoormalayeri@gmail.com>
2024-11-11 15:19:51 +0100

    first excercise

---
+++ excercise1.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = "Bahareh";
+$education = "Frontend Development";
+
+$what_i_love = ["Html","CSS","Vue","PHP"];
+
+echo "Hi! my name is $name, I'm studying $education and I love $what_i_love[0], $what_i_love[1], $what_i_love[2] and $what_i_love[3].";
+?>
