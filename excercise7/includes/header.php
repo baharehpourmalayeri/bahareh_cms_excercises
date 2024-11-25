53101157d4696286c7b521a74a28555c039d6074
baharehpourmalayeri <baharehpoormalayeri@gmail.com>
2024-11-25 11:59:42 +0100

    Set up basic structure

---
+++ excercise7/includes/header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="pages/index.php">Home</a></li>
+            <li><a href="pages/news.php">News</a></li>
+            <li><a href="pages/about.php">About</a></li>
+            <li><a href="pages/contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ excercise7/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>News Website</title>
+  <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+<?php include 'includes/header.php'; ?>
+
+    <main>
+        <h1>Welcome to the Next News</h1>
+        <p>Your one-stop source for the latest news.</p>
+    </main>
+
+    <?php include 'includes/footer.php'; ?>
+
+</body>
+</html>
